794895b139c740670e206ff721109d552e612f75
tranquocdong1 <tranquocdong10.2@gmail.com>
2025-10-04 11:45:02 +0700

    base

---
+++ backend/ltw2-project/src/main/java/com/example/ltw2_project/Ltw2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ltw2_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ltw2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ltw2ProjectApplication.class, args);
+	}
+
+}
---
+++ backend/ltw2-project/src/test/java/com/example/ltw2_project/Ltw2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ltw2_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ltw2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
